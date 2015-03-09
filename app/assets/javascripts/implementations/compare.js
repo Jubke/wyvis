90930bc5f1ddf2b0830146e48bebf42823b803b1
Jubke <luebke.julian@gmail.com>
2015-03-09 08:18:00 +0100

    resize in compare view

--- app/assets/javascripts/implementations/compare.js
+++ app/assets/javascripts/implementations/compare.js
@@ -2,4 +2,20 @@ var App = App || {};
 
 App.implementations.compare = function() {
   $( ".implementation" ).wyvis();
+
+  var container = $( ".implementation-container" );
+  container.delegate(".top-controls .btn-impl-enlarge", "click", function (e) {
+    $( e.currentTarget ).toggleClass("active");
+    $( e.delegateTarget ).toggleClass("full-width");
+
+    if ( container.not( e.delegateTarget ).is(":last-child") ) {
+      
+
+    } else {        
+      $( ".implementation-container" ).not( e.delegateTarget ).toggleClass("zero-width").delay(500).queue(function () {
+        // $( this ).toggle();
+        $( this ).dequeue();
+      }); 
+    }
+  });
 };
