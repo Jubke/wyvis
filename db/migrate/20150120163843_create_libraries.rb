147a7eb415960ae4726a831433e2038cb8f046f5
Jubke <luebke.julian@gmail.com>
2015-03-19 21:31:05 +0100

    new lib

---
+++ db/migrate/20150120163843_create_libraries.rb
@@ -0,0 +1,22 @@
+class CreateLibraries < ActiveRecord::Migration
+  def change
+    create_table :libraries do |t|
+      t.string :name, null: false
+      t.string :url, null: false
+      t.string :current_version
+      t.string :image_url
+      t.text :short
+      t.boolean :web_standard
+      t.string :render_tech
+      t.string :dependencies
+      t.string :license
+      t.string :support
+      t.date :first_release
+      t.date :latest_release
+      t.string :meta_tags
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
