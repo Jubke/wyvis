519d0069c124509b80a7fefe03cf0343a83cbfac
Jubke <luebke.julian@gmail.com>
2015-03-23 08:45:45 +0100

    Added compare empty support

--- app/assets/javascripts/implementations/compare.js
+++ app/assets/javascripts/implementations/compare.js
@@ -1,21 +1,30 @@
 var App = App || {};
 
 App.implementations.compare = function() {
-  $( ".implementation" ).wyvis();
+  // set the full window height
+  resizeFullHeight();
+  $( window ).on("resize", resizeFullHeight);
 
+  // initialize full-screen toggle
   var container = $( ".implementation-container" );
-  container.delegate(".top-controls .btn-impl-enlarge", "click", function (e) {
+  container.delegate(".top-controls .btn-full-screen", "click", toggleFullScreen);
+
+  /**
+   * Toggles the full-screen mode for an implementation container.
+   * @param  {Object} e event that caused the action
+   * @return {undefined}
+   */
+  function toggleFullScreen(e) {
     $( e.currentTarget ).toggleClass("active");
-    $( e.delegateTarget ).toggleClass("full-width");
+    $( e.delegateTarget ).toggleClass("full-screen");
+    container.not(e.delegateTarget).toggleClass("not-full-screen");
+  }
 
-    if ( container.not( e.delegateTarget ).is(":last-child") ) {
-      
-
-    } else {        
-      $( ".implementation-container" ).not( e.delegateTarget ).toggleClass("zero-width").delay(500).queue(function () {
-        // $( this ).toggle();
-        $( this ).dequeue();
-      }); 
-    }
-  });
+  /**
+   * Resizes the .compare-container element on a window resize.
+   * @return {undefined}
+   */
+  function resizeFullHeight() {
+    $( ".compare-container" ).height( $( window ).height() - $( ".navbar" ).height() );
+  }
 };
